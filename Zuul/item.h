ad68d01f936f2effbc948d74ec5ae0e64faa1b7b
Aneeq <412710@bsd48.org>
2022-01-05 21:14:02 -0700

    Oops forgot H

--- Zuul/item.h
+++ Zuul/item.h
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
-
+//Item class
 using namespace std;
 
 class item {
  public:
   char description[100];
   void printDescription();
-}
+};
--- Zuul/room.h
+++ Zuul/room.h
@@ -1,16 +1,23 @@
+#ifndef ITEM_H
+#define ITEM_H
 #include <iostream>
 #include <cstring>
 
 using namespace std;
-
 class room {
  public:
-  char description[100];
+  char description[700];
   void printDescription();
   bool haswest;
   bool haseast;
   bool hassouth;
   bool hasnorth;
   void printDirections();
-}
-
+  bool hasmask;
+  bool haslaptop;
+  bool hascharger;
+  bool hasswag;
+  bool hastrain;
+  void printItems();
+};
+#endif
